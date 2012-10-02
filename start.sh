b284f1b223274f764557476576c0ab1f6fa518d4
Graham Hay <grahamrhay@gmail.com>
2012-10-02 21:21:20 +0100

    Start mnesia with app

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s restbucks
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s mnesia start -s reloader -s restbucks
